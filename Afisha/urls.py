bec75da7142c55b30c382927c6d27c18345b76dd
MiamotoMusasi <urmatsh0608@gmail.com>
2023-02-23 17:24:26 +0600

    hw 2, 3

--- Afisha/urls.py
+++ Afisha/urls.py
@@ -15,14 +15,15 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from movie_app import views
+from movie_app.views import *
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/v1/directors/', views.director_list_api_view),
-    path('api/v1/directors/<int:id>/', views.director_detail_api_view),
-    path('api/v1/movies/', views.movie_list_api_view),
-    path('api/v1/movies/<int:id>/', views.movie_detail_api_view),
-    path('api/v1/reviews/', views.review_list_api_view),
-    path('api/v1/reviews/<int:id>/', views.review_detail_api_view),
+    path('api/v1/directors/', director_list_api_view),
+    path('api/v1/directors/<int:id>/', director_detail_api_view),
+    path('api/v1/movies/', movie_list_api_view),
+    path('api/v1/movies/<int:id>/', movie_detail_api_view),
+    path('api/v1/reviews/', review_list_api_view),
+    path('api/v1/reviews/<int:id>/', review_detail_api_view),
+    path('api/v1/movies/reviews/', review_movies_view),
 ]
