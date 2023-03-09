52a9b26e1514c71319307c478f247de90097b69a
MiamotoMusasi <urmatsh0608@gmail.com>
2023-03-10 02:48:55 +0600

    hw 7

--- Afisha/wsgi.py
+++ Afisha/wsgi.py
@@ -10,7 +10,9 @@ https://docs.djangoproject.com/en/4.1/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
+import dotenv
 
+dotenv.load_dotenv(os.path.join(os.path.dirname(os.path.dirname(__file__)), '.env'))
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Afisha.settings')
 
 application = get_wsgi_application()
